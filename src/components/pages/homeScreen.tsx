8e6feb4c38fe54f84ce6098930b7e42b0f0f9098
SeanDutt <seanwilcox@comcast.net>
2023-07-03 11:17:53 -0700

    another image change

--- src/components/pages/homeScreen.tsx
+++ src/components/pages/homeScreen.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Card from "../card.tsx";
-import inventoryIcon from "../../data/buildingIcons/inventory";
+import inventoryIcon from "../../data/buildingIcons/inventory.png";
 import workshopIcon from "../../data/buildingIcons/workshop";
-import exploreIcon from "../../data/exploreIcons/Forest";
-import requestIcon from "../../data/requestIcons/Woody";
-import buildingIcon from "../../data/buildingIcons/Tree Farm";
+// import exploreIcon from "../../data/exploreIcons/Forest";
+// import requestIcon from "../../data/requestIcons/Woody";
+// import buildingIcon from "../../data/buildingIcons/Tree Farm";
 
 export interface HomeScreenComponent {
   name: string;
@@ -23,21 +23,21 @@ const components: HomeScreenComponent[] = [
     text: "Craft things here!",
     imageUrl: workshopIcon,
   },
-  {
-    name: "Explore",
-    text: "Go on a resource run!",
-    imageUrl: exploreIcon,
-  },
-  {
-    name: "Requests",
-    text: "What do you want?",
-    imageUrl: requestIcon,
-  },
-  {
-    name: "Buildings",
-    text: "Manage automatic resources.",
-    imageUrl: buildingIcon,
-  },
+  // {
+  //   name: "Explore",
+  //   text: "Go on a resource run!",
+  //   imageUrl: exploreIcon,
+  // },
+  // {
+  //   name: "Requests",
+  //   text: "What do you want?",
+  //   imageUrl: requestIcon,
+  // },
+  // {
+  //   name: "Buildings",
+  //   text: "Manage automatic resources.",
+  //   imageUrl: buildingIcon,
+  // },
   // { name: 'Admin', text: 'Create things' },
   // { name: 'Register', text: '' },
   // Add more components to the array
