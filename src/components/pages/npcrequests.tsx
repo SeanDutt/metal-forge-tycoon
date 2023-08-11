f3393e7b467bc89eb8bc461de2ed2f5dce7111c4
SeanDutt <seanwilcox@comcast.net>
2023-08-11 15:05:36 -0700

    Testing card generation

--- src/components/pages/npcrequests.tsx
+++ src/components/pages/npcrequests.tsx
@@ -9,6 +9,26 @@ import { doesPlayerMeetRequirements } from "../../utils/requirements.tsx";
 const NpcRequests = () => {
   const player = useContext(PlayerContext);
   const [requests, setRequests] = useState<NpcRequest[]>([]);
+  const [requestCards, setRequestCards] = useState<React.ReactNode[]>([]);
+
+  const generateRequestCards = (preRequests: NpcRequest[]) => {
+    const cards = preRequests.map((request) => {
+      return (
+        <Card
+        key={request.name}
+        icon={
+          request
+            ? require(`../../data/requestIcons/${request.imageUrl}`)
+            : require(`../../data/itemIcons/noIcon.png`)
+        }
+        primaryText={request.name}
+        secondaryText={[`Request from ${request.from}`]}
+        link={`/Requests/${request.name}`}
+      />
+      )
+    }) 
+    setRequestCards(cards);
+  }
 
   const getRequestIndexInChain = (requestChainId: string): number => {
     const chainData = player.completedRequests?.[requestChainId];
@@ -55,21 +75,23 @@ const NpcRequests = () => {
       }
     };
     fetchRequests();
+    generateRequestCards(requests);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [player]);
 
+  if (!requestCards) {
+    // Handle case when requests data is generating
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h1>Requests</h1>
-      {requests.map((request) => (
-        <Card
-          key={request.name}
-          icon={require(`../../data/requestIcons/${request.imageUrl}`)}
-          primaryText={request.name}
-          secondaryText={[`Request from ${request.from}`]}
-          link={`/Requests/${request.name}`}
-        />
-      ))}
+      <h2>Inventory</h2>
+      {requestCards.length > 0 ? (
+        <div>{requestCards}</div>
+      ) : (
+        <p>No requests available.</p>
+      )}
     </div>
   );
 };
